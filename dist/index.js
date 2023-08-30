6d699ff3edc62eada9fc4f5e9e2daa6bd7267fba
Jake Daurham <daurham95@gmail.com>
2023-08-29 23:39:43 -0700

    scrapped extra file structure

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Modules
-// 
-// Add modules here...
-// 
-// export const LED = new GPIOModule(1, "out");
-// 
--- dist/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import the class
-// import { GPIOModule } from "./GPIOModule";
-// create a fake module
-// export const LED = new GPIOModule(1, "out");
-// toggle the value
-// LED.toggle();
